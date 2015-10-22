837221fd38d18db27e0534c43b3faeb9b9f13b3e
Chetan <chetanpawar0989@gmail.com>
2015-10-22 14:00:20 -0400

    Completing all redis tasks

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var express = require('express')
 var fs      = require('fs')
 var app = express()
 // REDIS
-//var client = redis.createClient(6379, '127.0.0.1', {})
+var client = redis.createClient(6379, '127.0.0.1', {})
+var urlList = "recentURLs";
+var imageQueue = "imageQueue";
 
 ///////////// WEB ROUTES
 
@@ -12,47 +14,98 @@ var app = express()
 app.use(function(req, res, next) 
 {
 	console.log(req.method, req.url);
+	client.lpush(urlList, req.url);
 
 	// ... INSERT HERE.
+	//client.lpush
 
 	next(); // Passing the request to the next handler in the stack.
 });
 
+app.get('/', function(req, res) {
+  res.send('hello world\n');
+})
 
-// app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
-//    console.log(req.body) // form fields
-//    console.log(req.files) // form files
+app.get('/set', function(req, res) {
+	var key = "key";
+	var value = "this message will self-destruct in 10 seconds";
+	client.set("key", value);
+	client.expire("key", 10);
+	res.send("value set");
+})
 
-//    if( req.files.image )
-//    {
-// 	   fs.readFile( req.files.image.path, function (err, data) {
-// 	  		if (err) throw err;
-// 	  		var img = new Buffer(data).toString('base64');
-// 	  		console.log(img);
-// 		});
-// 	}
+app.get('/get', function(req, res) {
+	client.get("key", function(err,value){ 
+		if(!err) {
+			console.log(value);
+			var redisvalue = value;	
+			res.send(redisvalue);
+		}
+		else{
+			res.send("no key found");
+		}	
+	});
+	//res.send(redisvalue);
+})
 
-//    res.status(204).end()
-// }]);
+app.get('/recent', function(req, res) {
+	
+	client.lrange(urlList, 0, 4, function(err,value){ 
+		if(!err) {
+			console.log(value);
+			var urlListValue = value;
+			var resText = "Recent URLs accessed:<br>";
+			for (var i = 0; i<urlListValue.length; i++) {
+				resText = resText + urlListValue[i] + "<br>";
+			}
+			res.send(resText);
+		}
+	});
+	
+})
+app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+   console.log(req.body) // form fields
+   console.log(req.files) // form files
 
-// app.get('/meow', function(req, res) {
-// 	{
-// 		if (err) throw err
-// 		res.writeHead(200, {'content-type':'text/html'});
-// 		items.forEach(function (imagedata) 
-// 		{
-//    		res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
-// 		});
-//    	res.end();
-// 	}
-// })
+   if( req.files.image )
+   {
+	   fs.readFile( req.files.image.path, function (err, data) {
+	  		if (err) throw err;
+	  		var img = new Buffer(data).toString('base64');
+	  		//console.log(img);
+	  		console.log("Image at " + req.files.image.path + " uploaded successfully");
+	  		client.lpush(imageQueue, img);
+		});
+	}
 
-// HTTP SERVER
-// var server = app.listen(3000, function () {
+   res.status(204).end()
+}]);
 
-//   var host = server.address().address
-//   var port = server.address().port
+app.get('/meow', function(req, res) {
+	{
+		client.ltrim(imageQueue, 0, 0, function(err, value) {
+			if (!err) {
+				client.lrange(imageQueue, 0, 0, function(err,items){ 
+					if(!err) {
+						res.writeHead(200, {'content-type':'text/html'});
+						items.forEach(function (imagedata)
+						{
+				   			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+						});
+					   	res.end();
+					}
+				});
+			}
+		});		
+	}
+})
 
-//   console.log('Example app listening at http://%s:%s', host, port)
-// })
+//HTTP SERVER
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+})
 
